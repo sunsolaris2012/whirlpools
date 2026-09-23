c62d9429e0ad9c3171725d7d5b9e12adad92eb65
omid Akbari <93394650+sunsolaris2012@users.noreply.github.com>
2026-09-23 11:41:39 +0330

    Create real_attack.rs

---
+++ programs/whirlpool/tests/real_attack.rs
@@ -0,0 +1,8 @@
+[dev-dependencies]
+solana-program-test = "=2.2.1"
+solana-sdk = "=2.2.1"
+solana-program = "=2.2.1"
+tokio = { version = "1.40", features = ["macros", "rt-multi-thread"] }
+spl-token = "=8.0.0"
+spl-token-2022 = "=8.0.1"
+sha2 = "0.10"
